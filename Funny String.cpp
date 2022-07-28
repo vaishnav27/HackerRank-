0d6db0e358b179f5dc0612dd6b9731fc9b7203e2
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-07-28 11:50:24 +0530

    Create Funny String.cpp

---
+++ Funny String.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+string funnyString(string s) {
+    string r;
+    r=s;
+    reverse(r.begin(),r.end());
+    int count=0;
+    for(int i=1;i<s.length();i++){
+       if (abs(s[i]-s[i-1])==abs(r[i]-r[i-1])) {
+          count++;
+       }
+    }
+    if (count==s.length()-1) {
+       return "Funny";
+    }else return "Not Funny";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = funnyString(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
